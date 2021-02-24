5e3950838829a6e105f2376c2062bce3a2520e30
ctalloc <cecilia.tallo@gmail.com>
2021-02-24 08:57:32 +0100

    First commit, components and folder structure created.

---
+++ src/components/PokeList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Pokemon from './Pokemon'
+
+class PokeList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Holi</p>
+            </div>
+        );
+    }
+}
+
+export default PokeList;
